24759aaf565bf3cbd064b8f57cde577e8586b503
Solar5503 <spidervvv@mail.ru>
2022-06-12 00:17:11 +0300

    add animation for cart button

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.scss';
@@ -8,14 +8,26 @@ interface IHeaderCartButtonProps {
 }
 
 const HeaderCartButton = ({ onClick }: IHeaderCartButtonProps) => {
+  const [btnIsBump, setBtnIsBump] = useState<boolean>(false);
   const { items } = useContext(CartContext);
 
   const numberOfCartItems = items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
 
+  const btnStyles: string = `${styles.button} ${btnIsBump ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+
+    setBtnIsBump(true);
+
+    const timer: NodeJS.Timeout = setTimeout(() => setBtnIsBump(false), 300);
+
+    return () => clearTimeout(timer);
+  }, [items]);
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnStyles} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
